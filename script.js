c8561171afba137f1fe4ee718232e23a1d647210
catherine-s25 <catherineshi2016@gmail.com>
2026-04-01 22:32:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,38 @@ document.getElementById("uploadForm").addEventListener("submit", async function(
       return;
     }
 
-    // Display rec nicely
-    summary.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+    // ----------------------------
+    // Option B: Display as tables
+    // ----------------------------
+    let html = "";
+
+    for (let cellType in data) {
+      html += `<h4>${cellType}</h4>`; // Header for this cell type
+
+      // Start table
+      html += "<table border='1' style='border-collapse: collapse;'>";
+      
+      // Table header
+      const firstRow = data[cellType][0];
+      html += "<tr>";
+      for (let col in firstRow) {
+        html += `<th style="padding: 4px; text-align: left;">${col}</th>`;
+      }
+      html += "</tr>";
+
+      // Table rows
+      for (let row of data[cellType]) {
+        html += "<tr>";
+        for (let col in row) {
+          html += `<td style="padding: 4px;">${row[col]}</td>`;
+        }
+        html += "</tr>";
+      }
+
+      html += "</table><br/>";
+    }
+
+    summary.innerHTML = html;
 
   } catch (err) {
     processing.classList.add("hidden");
